bc8dd38634ac29204554673297cbb65310506196
Tigran Najaryan <tigran@omnition.io>
2019-09-17 15:56:29 -0400

    Add OmnitelK Config and Factory
    Define OmnitelK exporter configuration and factory.
    The exporter itself is not yet implemented. It will
    be a future PR.

---
+++ exporter/omnitelk/config_test.go
@@ -0,0 +1,77 @@
+// Copyright 2019 Omnition Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package omnitelk
+
+import (
+	"path"
+	"testing"
+
+	"github.com/open-telemetry/opentelemetry-service/config"
+	"github.com/open-telemetry/opentelemetry-service/config/configmodels"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDefaultConfig(t *testing.T) {
+	factories, err := config.ExampleComponents()
+	assert.Nil(t, err)
+
+	factory := &Factory{}
+	factories.Exporters[factory.Type()] = factory
+	cfg, err := config.LoadConfigFile(
+		t, path.Join(".", "testdata", "default.yaml"), factories,
+	)
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	e := cfg.Exporters["omnitelk"]
+
+	assert.Equal(t, e,
+		&Config{
+			ExporterSettings: configmodels.ExporterSettings{
+				TypeVal: "omnitelk",
+				NameVal: "omnitelk",
+			},
+			Streams: 1,
+		},
+	)
+}
+
+func TestConfig(t *testing.T) {
+	factories, err := config.ExampleComponents()
+	assert.Nil(t, err)
+
+	factory := &Factory{}
+	factories.Exporters[factory.Type()] = factory
+	cfg, err := config.LoadConfigFile(
+		t, path.Join(".", "testdata", "config.yaml"), factories,
+	)
+
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	e := cfg.Exporters["omnitelk"]
+
+	assert.Equal(t, e,
+		&Config{
+			ExporterSettings: configmodels.ExporterSettings{
+				TypeVal: "omnitelk",
+				NameVal: "omnitelk",
+			},
+			Endpoint: "0.0.0.0:1234",
+			Streams:  456,
+		},
+	)
+}
---
+++ exporter/omnitelk/factory.go
@@ -0,0 +1,58 @@
+// Copyright 2019 Omnition Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package omnitelk
+
+import (
+	"github.com/open-telemetry/opentelemetry-service/config/configerror"
+	"github.com/open-telemetry/opentelemetry-service/config/configmodels"
+	"github.com/open-telemetry/opentelemetry-service/exporter"
+	"go.uber.org/zap"
+)
+
+const (
+	// The value of "type" key in configuration.
+	typeStr = "omnitelk"
+)
+
+// Factory is the factory for OmnitelK exporter.
+type Factory struct {
+}
+
+// Type gets the type of the Exporter config created by this factory.
+func (f *Factory) Type() string {
+	return typeStr
+}
+
+// CreateDefaultConfig creates the default configuration for exporter.
+func (f *Factory) CreateDefaultConfig() configmodels.Exporter {
+	return &Config{
+		Streams: 1,
+	}
+}
+
+// CreateTraceExporter initializes and returns a new trace exporter
+func (f *Factory) CreateTraceExporter(logger *zap.Logger, cfg configmodels.Exporter) (exporter.TraceExporter, error) {
+	// TODO: Create an exporter like this when Exporter and ClientImpl are ready.
+	// config := cfg.(*Config)
+	// e := NewExporter(config, logger, NewClient(config.Endpoint, config.Streams, logger))
+	// e.Start()
+	// return e, e.Stop, nil
+	return nil, configerror.ErrDataTypeIsNotSupported
+}
+
+// CreateMetricsExporter creates a metrics exporter based on this config.
+func (f *Factory) CreateMetricsExporter(logger *zap.Logger, cfg configmodels.Exporter) (exporter.MetricsExporter, error) {
+	return nil, configerror.ErrDataTypeIsNotSupported
+}
